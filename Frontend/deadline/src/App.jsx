7bb3bb29ce42e27c823d214d0c4b8398f295a87b
Niviane Alves <125082321+NivianeCas@users.noreply.github.com>
2026-05-21 09:33:33 -0300

    feat(dashboard-produtos): implementação do painel do lojista e catálogo de produtos (#32)
    * feat(dash): ajusta layout para grid e cores para verde institucional
    
    * Fix: atualização de rota

--- Frontend/deadline/src/App.jsx
+++ Frontend/deadline/src/App.jsx
@@ -2,22 +2,26 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import ClienteHome from './pages/ClienteHome';
 import AuthPage from './pages/AuthPage';
 import Dashboard from './pages/Dashboard';
+import Produtos from './pages/Produtos';
+
 
 export default function App() {
   return (
     <Routes>
       {/* Rota principal (Vitrine) */}
       <Route path="/" element={<ClienteHome />} />
-      
+
       {/* Rota unificada de Autenticação (Double Slider) */}
       <Route path="/auth" element={<AuthPage />} />
-      
+
       {/* Redirecionamentos de segurança para links antigos */}
       <Route path="/login" element={<Navigate to="/auth" replace />} />
       <Route path="/cadastro" element={<Navigate to="/auth" replace />} />
-      
+
       {/* Painel Administrativo da Empresa */}
       <Route path="/dashboard" element={<Dashboard />} />
+
+      <Route path="/produtos" element={<Produtos />} />
     </Routes>
   );
 }
