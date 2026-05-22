d1a9c11dff85b053718b5aceeee8fe0661d5efbb
AlaneOLiveira <maria.alane404dev@gmail.com>
2026-05-22 20:36:36 -0300

    fix: remove estrutura aninhada de src/pages

--- Frontend/deadline/src/pages/Backend/deadline/src/main/java/com/synapse/deadline/exceptions/CredenciaisInvalidasException.java
+++
@@ -1,7 +0,0 @@
-package com.synapse.deadline.exceptions;
-
-public class CredenciaisInvalidasException extends RuntimeException {
-    public CredenciaisInvalidasException() {
-        super("E-mail ou senha inválidos."); // Mensagem genérica PROPOSITAL para segurança
-    }
-}
